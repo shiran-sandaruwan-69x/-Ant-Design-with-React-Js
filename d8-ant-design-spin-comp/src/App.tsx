b71ad7e4436371e9ae3a001f3e06f693d2f1430a
shiran-sandaruwan-69x <shiran101997@gmail.com>
2023-04-26 13:10:59 +0530

    coverd ant design spin component

---
+++ d8-ant-design-spin-comp/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'antd/dist/reset.css';
+import SpinComp from "./component/SpinComp";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <SpinComp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ d8-ant-design-spin-comp/src/component/SpinComp.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Button, Spin, Typography} from "antd";
+import './css/style.css'
+interface SpinProps {
+
+}
+interface SpinState {
+    isSpin:boolean
+}
+class SpinComp extends React.Component<SpinProps,SpinState> {
+
+    constructor(props: Readonly<SpinProps> | SpinProps) {
+        super(props);
+        this.state={
+            isSpin:true
+        }
+    }
+
+    setSpin=()=>{
+        this.state.isSpin ? this.setState({isSpin:false}) : this.setState({isSpin:true})
+    }
+
+    render() {
+        return (
+         <div className='loginForm'>
+             <Spin
+                 spinning={this.state.isSpin}>
+             </Spin>
+             <Button type='primary' onClick={this.setSpin}>spin button</Button>
+             <Spin spinning={this.state.isSpin}>
+                 <p>apple</p>
+                 <p>banana</p>
+                 <p>cake</p>
+                 <p>pine apple</p>
+             </Spin>
+
+         </div>
+        );
+    }
+}
+
+export default SpinComp;
