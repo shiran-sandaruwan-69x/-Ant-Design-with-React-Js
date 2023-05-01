7eae65c6d3d0bc4ee3c5a1014423fbe40bfd60ef
shiran-sandaruwan-69x <shiran101997@gmail.com>
2023-05-01 19:31:36 +0530

    coverd ant design steps component

---
+++ d25-ant-design-steps-comp/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TaskProgressComp from "./component/TaskProgressComp";
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <TaskProgressComp/>
+    </div>
+  );
+}
+
+export default App;
