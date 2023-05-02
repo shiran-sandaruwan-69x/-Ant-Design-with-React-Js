fd1d32f5dd27cdfb1d4a1890e7fd93739a48a4fa
shiran-sandaruwan-69x <shiran101997@gmail.com>
2023-05-02 11:56:10 +0530

    coverd ant design side menu and routes

---
+++ d28-ant-design-side-menu-and-routes-comp/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RouteAntDesignComp from "./component/RouteAntDesignComp";
+
+
+function App() {
+  return (
+    <div className="App">
+        <RouteAntDesignComp/>
+    </div>
+  );
+}
+
+export default App;
